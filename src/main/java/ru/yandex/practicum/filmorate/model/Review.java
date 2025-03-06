bf685053be3133630ebd9522d4b1a8cee6746892
Sergey Filippovskikh <me@filippovskikh.ru>
2025-03-06 18:16:34 +0300

    1.Создана модель Отзывов Review 2. Добавлены 2 таблицы в базу данных: --- reviews - для отзывов --- feedbacks - оценки отзывов 4. Создан Репозиторий 5. Созданы сервисы 6. Подготовлен контроллер

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.boot.context.properties.bind.Name;
 import ru.yandex.practicum.filmorate.validator.Marker;
 
 /**
