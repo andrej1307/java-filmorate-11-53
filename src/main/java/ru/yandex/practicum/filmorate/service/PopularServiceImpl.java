a9d492d358a8372e73352797b02ec0fb034c9945
Sergey Filippovskikh <116564864+SergikF@users.noreply.github.com>
2025-03-09 13:27:53 +0300

    Вывод самых популярных фильмов по жанру и годам. add-most-populars Новые базы не создавал. Напрямую к таблицам не обращался. Использовал существующие возможности.

--- src/main/java/ru/yandex/practicum/filmorate/service/PopularServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/PopularServiceImpl.java
@@ -5,10 +5,8 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,30 +17,32 @@ public class PopularServiceImpl implements PopularService {
     @Override
     public List<Film> getPopular(Integer year, Integer genreId, Integer count) {
 
-        // получаем из базы список всех фильмов
-        Collection<Film> draftFilms = films.findAllFilms();
+// получаем из базы список всех фильмов
+        Collection<Film> listFilms = films.findAllFilms();
 
-        // создаём новый список фильмов, но уже с жанрами
-        Collection<Film> genreFilms = new ArrayList<>();
-        draftFilms.forEach(film -> {
-              genreFilms.add(films.getFilmById(film.getId()).get());
-        });
+// добавляем жанры к фильмам
+        listFilms.forEach(film -> film.setGenres(films.getFilmById(film.getId()).get().getGenres()));
 
-        // делаем выборку по году и жанру
-        List<Film> listFilms = genreFilms.stream()
-                .filter(film ->
-                        Optional.ofNullable(year).map(y ->
-                                film.getReleaseDate().getYear() == y).orElse(true)
-                     && Optional.ofNullable(genreId).map(g ->
-                                film.getGenres().stream().anyMatch(genre ->
-                                        genre.getId() == g)).orElse(true))
-                .toList();
+// фильтруем фильмы по году, если указан
+        if (year != null) {
+            listFilms = listFilms.stream()
+                    .filter(film -> film.getReleaseDate().getYear() == year)
+                    .toList();
+        }
 
-        // сортируем список по рейтингу и возвращаем необходимое количество фильмов
+// фильтруем фильмы по жанру, если указан
+        if (genreId != null) {
+            listFilms = listFilms.stream()
+                    .filter(film -> film.getGenres()
+                            .stream().anyMatch(genre -> genre.getId() == genreId))
+                    .toList();
+        }
+
+// сортируем фильмы по рейтингу и возвращаем необходимое количество фильмов
         return listFilms.stream()
-                .sorted((film1, film2) ->
-                        films.getFilmRank(film2.getId()).compareTo(films.getFilmRank(film1.getId())))
-                        .limit(Optional.ofNullable(count).orElse(Integer.MAX_VALUE))
-                .toList();
+                .sorted(Comparator.comparing(film ->
+                        films.getFilmRank(film.getId()), Comparator.reverseOrder()))
+                .limit(Optional.ofNullable(count).orElse(Integer.MAX_VALUE))
+                .collect(Collectors.toList());
     }
 }
